54549f3082210583bcfc7112a25b47a746104d3f
David Johnston <dvdjoh@gmail.com>
2023-02-19 11:37:18 -0500

    Add tests and contracts to jsonic

--- beautiful_racket/jsonic-lang/jsonic/tokenizer.rkt
+++ beautiful_racket/jsonic-lang/jsonic/tokenizer.rkt
@@ -1,5 +1,14 @@
 #lang br/quicklang
-(require brag/support)
+(require brag/support racket/contract)
+(module+ test (require rackunit))
+
+(define (jsonic-token? x)
+  (or (eof-object? x) (token-struct? x)))
+
+(module+ test
+  (check-true (jsonic-token? eof))
+  (check-true (jsonic-token? (token 'A-TOKEN "hello")))
+  (check-false (jsonic-token? 42)))
 
 (define (make-tokenizer port)
   (define (next-token)
@@ -11,4 +20,17 @@
        [any-char (token 'CHAR-TOK lexeme)]))
     (jsonic-lexer port))
   next-token)
-(provide make-tokenizer)
+(provide (contract-out
+          [make-tokenizer (input-port? . -> . (-> jsonic-token?))]))
+
+(module+ test
+  (check-equal?
+   (apply-tokenizer-maker make-tokenizer "// comment\n")
+   empty)
+  (check-equal?
+   (apply-tokenizer-maker make-tokenizer "@$ (+ 6 7) $@")
+   (list (token-struct 'SEXP-TOK " (+ 6 7) " #f #f #f #f #f)))
+  (check-equal?
+   (apply-tokenizer-maker make-tokenizer "hi")
+   (list (token-struct 'CHAR-TOK "h" #f #f #f #f #f)
+         (token-struct 'CHAR-TOK "i" #f #f #f #f #f))))
